88cd5da2054d14a7cffd14425782483eed59b905
gabcont <dntgvup@proton.me>
2025-03-15 16:59:03 -0400

    actualizado diario

--- scripts/personaje.gd
+++ scripts/personaje.gd
@@ -4,12 +4,12 @@ extends Node2D
 @onready var animacion_player : AnimationPlayer = $AnimationPlayer
 
 func play_animacion(animacion : String) -> void:
-    match animacion:
-        "huida":
-            animacion_player.play(animacion)
-        
-    sprite.play(animacion)
-    $Sonido.play_sfx(animacion)
+	match animacion:
+		"huida":
+			animacion_player.play(animacion)
+		
+	sprite.play(animacion)
+	$Sonido.play_sfx(animacion)
 
 func _on_animated_sprite_2d_animation_finished() -> void:
 	sprite.play("idle")
