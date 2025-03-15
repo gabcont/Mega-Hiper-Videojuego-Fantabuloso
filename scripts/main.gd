3c23c790d5ead9078f8cfe69683e50bff099833f
Guito22 <elguito2003@gmail.com>
2025-03-15 14:35:20 -0400

    Cambio de duracion de frame de ataque 2 y frames de idle

--- scripts/main.gd
+++ scripts/main.gd
@@ -3,10 +3,10 @@ extends Node2D
 @onready var personaje = get_node("Personaje")
 
 func _ready() -> void:
-    $BotonAtaque1.connect("boton_presionado", _on_boton_presionado)
-    $BotonAtaque2.connect("boton_presionado", _on_boton_presionado)
-    $BotonAtaque3.connect("boton_presionado", _on_boton_presionado)
-    $BotonAtaque4.connect("boton_presionado", _on_boton_presionado)
+	$BotonAtaque1.connect("boton_presionado", _on_boton_presionado)
+	$BotonAtaque2.connect("boton_presionado", _on_boton_presionado)
+	$BotonAtaque3.connect("boton_presionado", _on_boton_presionado)
+	$BotonAtaque4.connect("boton_presionado", _on_boton_presionado)
 
 func _on_boton_presionado(nombre_boton : String):
-    personaje.play_animacion(nombre_boton)
+	personaje.play_animacion(nombre_boton)
--- scripts/sonido.gd
+++ scripts/sonido.gd
@@ -2,12 +2,12 @@ class_name SFX extends Node
 
 
 func play_sfx(audio : String) -> void:
-    match audio:
-        "ataque_1":
-            $ataque_1.play()
-        "ataque_2":
-            $ataque_2.play()
-        "ataque_especial":
-            $ataque_especial.play()
-        "huida":
-            $huida.play()
+	match audio:
+		"ataque_1":
+			$ataque_1.play()
+		"ataque_2":
+			$ataque_2.play()
+		"ataque_especial":
+			$ataque_especial.play()
+		"huida":
+			$huida.play()
