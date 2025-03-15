d1c8837bb4f89a0e26b2d1ec064cf7dbb25eaa46
gabcont <dntgvup@proton.me>
2025-03-15 14:15:55 -0400

    Revertir a animación anterior

--- scripts/personaje.gd
+++ scripts/personaje.gd
@@ -7,8 +7,8 @@ func play_animacion(animacion : String) -> void:
     match animacion:
         "huida":
             animacion_player.play(animacion)
-        _:
-            sprite.play(animacion)
+        
+    sprite.play(animacion)
     $Sonido.play_sfx(animacion)
 
 func _on_animated_sprite_2d_animation_finished() -> void:
