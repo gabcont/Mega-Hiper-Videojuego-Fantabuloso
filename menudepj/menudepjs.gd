3cce79cc260cda1a4531bdd024ea7c36f65ff656
Gearing123 <ingworkspace37@gmail.com>
2025-03-17 18:03:02 -0400

    menu de pj
    menu de pj

---
+++ menudepj/menudepjs.gd
@@ -0,0 +1,36 @@
+extends CanvasLayer
+
+const personajes = [
+	preload("res://Knuckles.png"),
+	preload("res://Kirby.png"),
+	preload("res://Sans.png"),
+	preload("res://Luffy.png")
+]
+var index_selection = 2
+var personaje_sprite
+
+func _ready():
+	personaje_sprite = $Sprite2D
+	update_portrait(index_selection)
+	
+func update_portrait(index):
+	personaje_sprite.texture = personajes[index]
+
+func _on_button_izq_pressed() -> void:
+	if(index_selection > 0 ):
+		index_selection -= 1
+	elif(index_selection == 0):
+		index_selection = personajes.size()-1
+	update_portrait(index_selection)
+
+
+func _on_button_der_pressed() -> void:
+	if(index_selection < personajes.size()-1):
+		index_selection +=1
+	elif(index_selection == personajes.size()-1 ):
+		index_selection = 0
+	update_portrait(index_selection)
+
+
+func _on_button_ok_pressed() -> void:
+	print("texto")
