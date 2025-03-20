ab752408da53a57583fb047d0174729a8e6f32b8
gabcont <dntgvup@proton.me>
2025-03-20 09:23:21 -0400

    solucionado loop en pantalla final

--- menu_final_partida/scripts/menu_final.gd
+++ menu_final_partida/scripts/menu_final.gd
@@ -1,14 +1,21 @@
 extends Button
 
 # Ruta a la escena que quieres cargar
-@export var escena_destino: PackedScene
+var escena_juego = load("res://gameplay/escenas/partida.tscn")
+var escena_menu = load("res://menu-principal/escenas/menu_principal.tscn")
 
 func _ready():
 	# Conecta la señal "pressed" a la función "on_button_pressed"
 	pressed.connect(on_button_pressed)
 
 func on_button_pressed():
-	# Cambia a la escena de destino
 	Transicion.transicion_partida()
 	await Transicion.on_transition_finished
-	get_tree().change_scene_to_packed(escena_destino)
+
+	if text == "VOLVER A JUGAR":
+		# Cambia a la escena de destino
+		get_tree().change_scene_to_file(escena_juego.resource_path)
+	else:
+		get_tree().change_scene_to_file(escena_menu.resource_path)
+	
+	
