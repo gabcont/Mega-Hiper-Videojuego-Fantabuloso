21b0d0c686abde4e0fdbcd98fe0b0e5b0b630343
gabcont <dntgvup@proton.me>
2025-03-21 23:17:32 -0400

    nueva estructura de archivos, nuevo modelo de menu

--- gameplay/escenas/musica.gd
+++
@@ -1,5 +0,0 @@
-extends AudioStreamPlayer  # Asegúrate de que el script esté asignado al nodo AudioStreamPlayer
-
-func _ready():
-	# Configura el nodo de música para ignorar la pausa del árbol
-	self.pause_mode = Node.PAUSE_MODE_PROCESS
